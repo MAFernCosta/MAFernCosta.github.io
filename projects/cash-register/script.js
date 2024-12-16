d28f38e12b0cf6226ba747d9c3d22352ce628e93
Havrick <asrahdr@gmail.com>
2024-12-16 18:54:35 +0100

    Cash regiter project updated

--- projects/cash-register/script.js
+++ projects/cash-register/script.js
@@ -10,18 +10,20 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+//[["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
+//[["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
 
 const cashElement = document.getElementById("cash");
 const changeDrawerElement = document.getElementById("change-drawer");
 const changeDueElement = document.getElementById("change-due");
-const payBtn = document.getElementById("pay-button");
+const payBtn = document.getElementById("purchase-btn");
 const priceElement = document.getElementById("price");
 priceElement.textContent = price;
 const totalCashInDrawerElement = document.getElementById("total-cash-in-drawer");
 
 const curr = [
   ["Penny", 0.01],
-  ["Nickel", 0.02],
+  ["Nickel", 0.05],
   ["Dime", 0.1],
   ["Quarter", 0.25],
   ["Dollar", 1],
@@ -30,6 +32,7 @@ const curr = [
   ["Twenty Dollars", 20],
   ["One Hundred Dollars", 100]
 ];
+
 
 class Money{
    constructor(currecyUnit, value, name, amount) {
@@ -44,37 +47,55 @@ class Money{
 class CashRegister{
   constructor(money){
     this.cashInDrawer = money;
+    this.currChar = "$";
   }
   showCashInDrawer(){
     let result = "";
-    this.cashInDrawer.map(el => result +=`<li>${el.currecyUnit}: ${el.amount}</li>`)
+    this.cashInDrawer.map(el => result +=`${el.currecyUnit}: ${this.currChar}${el.amount} </br>`)
     return result;
   }
   totalCashInDrawer(){
-    return this.cashInDrawer.map((curr) => curr.amount).reduce((total, currentValue) => total + currentValue ,0);
+    return Math.round((this.cashInDrawer.map((curr) => curr.amount).reduce((total, currentValue) => total + currentValue ,0)) * 100) / 100;
   }
   makeChange(cash, price){
-    let changeValue = (cash - price).toFixed(2);
-    console.log(changeValue); //DEBUG
+    let changeValue = Math.round((cash - price) * 100) / 100;
+    let result = this.checkTransaction(changeValue, price);
     let change = {};
+    if(result){
+      return result;
+    }
+    //console.log(changeValue); //DEBUG
     while (changeValue > 0){ 
+
+      //console.log("Change" + changeValue); //DEBUG
       this.cashInDrawer.toReversed().find((item) => {
-        if(changeValue >= item.value){
-          console.log(changeValue, item.value); //DEBUG
-          change[item.currecyUnit] = change[item.currecyUnit] ? change[item.currecyUnit] + item.value : item.value;
-          changeValue = (changeValue - item.value).toFixed(2);
-          item.amount = (item.amount - item.value).toFixed(2);       
-          console.log(item.amount)   //DEBUG
+        if(changeValue >= item.value && item.amount > 0){
+          change[item.name] = change[item.name] ? Math.round((change[item.name] + item.value) * 100) / 100 : item.value;
+          changeValue = Math.round((changeValue - item.value) * 100) / 100;
+          item.amount = Math.round((item.amount - item.value) * 100) / 100;     
+          //console.log(changeValue, item.amount, item.value, item.name); //DEBUG
           return true;
         } 
       });
     }
-    let result = "<ul>";
-    for(var key in change){
-      result += `<li>${key}: ${change[key]}</li>`;
+
+    result = `Status: ${this.totalCashInDrawer() <= 0 ? "CLOSED " : "OPEN "}`;
+    if(cash !== price){
+      for(var key in change){
+        result += `${key}: ${this.currChar}${change[key]} `;
+      }
+    } else {
+      result = "No change due - customer paid with exact cash";
     }
-    result += "</ul>";
-    return result;
+    return result;   
+  }
+  checkTransaction(change, price){
+    const drawerCash = this.cashInDrawer.toReversed().filter((item) => change >= item.value).reduce((acc, currVal) => acc + currVal.amount ,0);
+    //this.cashInDrawer.toReversed().filter((item) => change >= item.value).forEach((el) => console.log(`${el.name} : ${el.amount}`));
+    //console.log(`drawerCash: ${drawerCash}, change: ${change}`); //debug
+    if (drawerCash < change){
+      return "Status: INSUFFICIENT_FUNDS";
+    }
   }
 }
 
@@ -87,15 +108,12 @@ totalCashInDrawerElement.textContent = cashregister.totalCashInDrawer();
 payBtn.addEventListener("click", ()=>{
   
   if(cashElement.value){
-    const payment = parseInt(cashElement.value);
+    const payment = Number(cashElement.value);
     if(payment < price){
-      alert("Customer doesnt have enought money to buy the item.")
-      return;
-    } else if(payment > cashregister.totalCashInDrawer()){
-      changeDueElement.innerHTML = "Status: INSUFFICIENT_FUNDS";
+      alert("Customer does not have enough money to purchase the item")
       return;
     } else {
-      changeDueElement.innerHTML = cashregister.makeChange(cashElement.value, price);
+      changeDueElement.textContent = cashregister.makeChange(payment, price);
       changeDrawerElement.innerHTML = cashregister.showCashInDrawer();
       totalCashInDrawerElement.textContent = cashregister.totalCashInDrawer();
     }
